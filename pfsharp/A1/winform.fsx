c0466aaa578a19e984d653e22813c901f96d2545
ueki <ueki005@gmail.com>
2011-01-31 13:05:08 +0900

    u

--- pfsharp/A1/winform.fsx
+++ pfsharp/A1/winform.fsx
@@ -5,7 +5,7 @@ open System.Windows.Forms
 let form = new Form()
 let btn = new Button(Text = "Click Me")
 btn.Click.AddHandler(fun _ _ ->
-  MessageBox.Show("Hello, World")
+  MessageBox.Show("Hello, Worldだ！")
   |> ignore)
 form.Controls.Add(btn)
 form.ShowDialog()
